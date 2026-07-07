735c944a16f45bfd3f7522313a00ac40df02e5f1
Edvan Brito <edvanbrito.j@hotmail.com>
2026-07-07 13:27:33 -0300

    chore: initial project setup

---
+++ backend/src/main/java/br/com/edvan/loan_calculator_api/LoanCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.edvan.loan_calculator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCalculatorApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/edvan/loan_calculator_api/LoanCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edvan.loan_calculator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
